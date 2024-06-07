e83f0fc6090822d5104ab8bf1d14bf47e4b47021
Lucho8 <lucianogonzalofredes@gmail.com>
2024-06-07 17:32:48 -0300

    completed

---
+++ src/projectsData/projectsData.js
@@ -0,0 +1,14 @@
+const projectsData = [
+  {
+    id: 1,
+    title: "Videogames Page",
+    description:
+      "Website in where that you can search for in a diverse Database and also add games.",
+    image: "/images/projects/Videogames-app.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+];
+
+export default projectsData;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -12,6 +14,11 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    colors:{
+      ...colors,
+      primary: colors.red,
+      secondary: colors.black,
     },
   },
   plugins: [],
